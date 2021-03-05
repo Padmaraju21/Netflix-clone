df08906798fb10bc33657111eb581c19797a8a52
pencil <penciljamnoreply@gmail.com>
2021-03-05 14:35:47 +0000

    fixed 400 error

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -1,9 +1,8 @@
 import Firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
-
-// 1) when seeding the database you'll have to uncomment this!
 //import { seedDatabase } from '../seed';
+
 
 const config = {
     apiKey: "AIzaSyAJdlatrM27a4mSu0WUpqH-J8EjYIIVLR0",
@@ -16,8 +15,5 @@ const config = {
     measurementId: "G-VHHWM7P45W"
   };
 const firebase = Firebase.initializeApp(config);
-// 2) when seeding the database you'll have to uncomment this!
-// seedDatabase(firebase);
-// 3) once you have populated the database (only run once!), re-comment this so you don't get duplicate data
-
+//seedDatabase(firebase);
 export { firebase };
